45539018c19d926e00ca20aafb79745ea9304446
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-08 04:54:51 +0530

    first commit

---
+++ src/main/java/com/nest/shoppingcartapp_backend/controller/ShoppingController.java
@@ -0,0 +1,10 @@
+package com.nest.shoppingcartapp_backend.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShoppingController {
+
+
+}
