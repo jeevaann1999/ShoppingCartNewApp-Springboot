3c8f554606e06cba8c4c62370cc8dc0fa3c0b1c8
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-11 15:17:57 +0530

    userlogin

--- src/main/java/com/nest/shoppingcartapp_backend/controller/ShoppingController.java
+++ src/main/java/com/nest/shoppingcartapp_backend/controller/ShoppingController.java
@@ -8,6 +8,7 @@ import com.nest.shoppingcartapp_backend.model.UserRegister;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -17,6 +18,26 @@ public class ShoppingController {
 
     @Autowired
     private RegisterDao d;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/userlogin",consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> UserLogin(@RequestBody UserRegister ul){
+        System.out.println(ul.getEmail());
+        List<UserRegister> result= (List<UserRegister>) d.FindUserLogin(ul.getEmail(),ul.getPassword());
+        System.out.println(result);
+        HashMap<String,String> st=new HashMap<>();
+        if(result.size()==0) {
+            st.put("status", "failed");
+            st.put("message", "user doesn't exist");
+        }
+        else {
+            int id =result.get(0).getId();
+            st.put("userId",String.valueOf(id));
+            st.put("status", "success");
+            st.put("message", "user login success");
+        }
+        return st;
+    }
 
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/add",consumes = "application/json", produces = "application/json")
--- src/main/java/com/nest/shoppingcartapp_backend/dao/RegisterDao.java
+++ src/main/java/com/nest/shoppingcartapp_backend/dao/RegisterDao.java
@@ -1,7 +1,13 @@
 package com.nest.shoppingcartapp_backend.dao;
 
 import com.nest.shoppingcartapp_backend.model.UserRegister;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RegisterDao extends CrudRepository<UserRegister, Integer> {
+    @Query(value ="SELECT `id`, `address`, `confirm`, `email`, `name`, `password`, `phone` FROM `user` WHERE `email`=:email AND `password`=:password",nativeQuery = true)
+    List<UserRegister> FindUserLogin(@Param("email") String email,@Param("password") String password);
 }
